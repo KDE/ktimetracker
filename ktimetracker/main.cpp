492f6a83362f36dc139ec0d95413597edfacb4c2
Waldo Bastian <bastian@kde.org>
2000-01-16 15:22:38 +0000

    WABA: Use KCmdLineArgs
    svn path=/trunk/kdeutils/karm/; revision=38492

--- ktimetracker/main.cpp
+++ ktimetracker/main.cpp
@@ -1,20 +1,27 @@
-
 #include <kapp.h>
+#include <klocale.h>
+#include <kcmdlineargs.h>
 
 #include "top.h"
 
+
+static const char *description = 
+	I18N_NOOP("KDE Time tracker tool.");
+
+static const char *version = "v0.0.1";
+
+
 int main( int argc, char *argv[] )
 {
-	KApplication *myApp = new KApplication( argc, argv, "karm" );
+	KCmdLineArgs::init(argc, argv, "karm", description, version);
+	KApplication myApp;
+
 	KarmWindow *karm = new KarmWindow;
 
-	myApp->setMainWidget( karm );
+	myApp.setMainWidget( karm );
 	karm->show();
 	
-	int ret = myApp->exec();
-
-	delete karm;
-	delete myApp;
+	int ret = myApp.exec();
 
 	return ret;
 }
