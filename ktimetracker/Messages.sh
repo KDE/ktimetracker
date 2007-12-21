c1ef4e391360bbb871d25a92f75a5ef20f82ab50
Thomas Reitelbach <tr@erdfunkstelle.de>
2007-12-21 21:24:19 +0000

    Catch all i18n strings for translation
    CCMAIL:kde-i18n-doc@kde.org
    
    svn path=/trunk/KDE/kdepim/ktimetracker/; revision=751443

--- ktimetracker/Messages.sh
+++ ktimetracker/Messages.sh
@@ -1,2 +1,5 @@
 #! /bin/sh
+$EXTRACTRC `find . -name \*.ui` >>  rc.cpp
+$EXTRACTRC `find . -name \*.rc` >> rc.cpp
+$EXTRACTRC `find . -name \*.kcfg` >> rc.cpp
 $XGETTEXT *.cpp -o $podir/ktimetracker.pot
