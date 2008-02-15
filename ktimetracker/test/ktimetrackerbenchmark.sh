41558fcac5435c358487411c53f8acf5f7473d4c
Thorsten Staerk <dev@staerk.de>
2008-02-15 19:36:48 +0000

    improving benchmark
    svn path=/trunk/KDE/kdepim/ktimetracker/; revision=775419

--- ktimetracker/test/ktimetrackerbenchmark.sh
+++ ktimetracker/test/ktimetrackerbenchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This is a bash script for a ktimetracker benchmark - how fast is ktimetracker on your system ?
-# example: my 2.4GHz Quad-core X64 computer with 4GB RAM needs 20 seconds
+# example: my 2.4GHz Quad-core X64 computer with 4GB RAM needs 50 seconds
 
 # preparation
 killall ktimetracker
@@ -14,4 +14,7 @@ time(
   
   # add 300 tasks
   for i in $(seq 1 1 300); do qdbus org.kde.ktimetracker /KTimeTracker addTask task$i; done 
+  qdbus org.kde.ktimetracker /KTimeTracker org.kde.ktimetracker.ktimetracker.saveAll
+  for i in $(seq 1 1 300); do qdbus org.kde.ktimetracker /KTimeTracker org.kde.ktimetracker.ktimetracker.startTimerFor $(qdbus org.kde.ktimetracker /KTimeTracker org.kde.ktimetracker.ktimetracker.taskIdsFromName task$i); done  
+  qdbus org.kde.ktimetracker /KTimeTracker  org.kde.ktimetracker.ktimetracker.stopAllTimersDBUS
 )
