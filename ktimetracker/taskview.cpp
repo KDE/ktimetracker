73702e32c22773d199f809ab10c84687110594fc
Thomas McGuire <mcguire@kde.org>
2008-07-19 18:27:40 +0000

    Merged revisions 830758 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/trunk/KDE/kdepim
    ........
      r830758 | tstaerk | 2008-07-11 09:28:42 +0200 (Fri, 11 Jul 2008) | 4 lines
      
      Change deleting a task so that ktimetracker neither crashes nor folds
      all tasks.
      CCBUGS:158743 166107
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=834940

--- ktimetracker/taskview.cpp
+++ ktimetracker/taskview.cpp
@@ -927,6 +927,7 @@ void TaskView::deleteTaskBatch( Task* task )
     _idleTimeDetector->stopIdleDetection();
     emit timersInactive();
   }
+  task->delete_recursive();
   emit tasksChanged( d->mActiveTasks );
 }
 
@@ -965,7 +966,7 @@ void TaskView::deleteTask( Task* task )
     for (int n=delendum.size()-1; n>=0; --n) 
       deleteTaskBatch(itemAt(delendum[n]));
   }
-  d->mStorage->buildTaskView(this);
+  //d->mStorage->buildTaskView(this);
 }
 
 void TaskView::markTaskAsComplete()
