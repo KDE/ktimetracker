b28642f43239e8d493fe77a3ac451d29d0fdb247
Allen Winter <winter@kde.org>
2009-07-06 12:41:34 +0000

    backport SVN commit 991853 by pokrzywka:
    Windows compatibility: use QUrl::toLocalFile() (which returns "C:/foo") instead of QUrl::path() (which returns "/C:/foo")
    
    
    svn path=/branches/KDE/4.3/kdepim/; revision=992178

--- ktimetracker/timetrackerwidget.cpp
+++ ktimetracker/timetrackerwidget.cpp
@@ -427,7 +427,7 @@ void TimetrackerWidget::openFile( const QString &fileName )
 
 void TimetrackerWidget::openFile( const KUrl &fileName )
 {
-  openFile( fileName.path() );
+  openFile( fileName.toLocalFile() );
 }
 
 bool TimetrackerWidget::closeFile()
