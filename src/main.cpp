8f7fb38f60bc8b4d4d6736fcc8aebec7c9e284e1
Friedrich W. H. Kossebau <kossebau@kde.org>
2019-11-19 09:19:19 +0100

    Set KI18n app translation domain

--- src/main.cpp
+++ src/main.cpp
@@ -82,6 +82,8 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     Q_INIT_RESOURCE(ktimetracker);
 
+    KLocalizedString::setApplicationDomain("ktimetracker");
+
     KAboutData aboutData(
         QStringLiteral("ktimetracker"),
         i18n("KTimeTracker"),
