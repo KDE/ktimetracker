426d362cf6b0b1226392d0606780f637349db852
Pino Toscano <pino@kde.org>
2019-11-19 13:13:06 +0100

    i18n: remove rc.cpp only after xgettext
    This way, the messages in it are actually used.

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-rm -f rc.cpp
 $EXTRACTRC `find . -name \*.ui` >> rc.cpp
 $EXTRACTRC `find . -name \*.rc` >> rc.cpp
 $EXTRACTRC `find . -name \*.kcfg` >> rc.cpp
 $XGETTEXT `find . -name \*.h -o -name \*.cpp` -o $podir/ktimetracker.pot
+rm -f rc.cpp
