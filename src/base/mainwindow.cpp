0afbcf3532504e2b50c18c3ef7c8557aa6bbf9d6
Thiago Sueto <herzenschein@gmail.com>
2024-03-06 15:44:10 -0300

    Workaround for tray close bug
    https://bugs.kde.org/show_bug.cgi?id=478734

--- src/base/mainwindow.cpp
+++ src/base/mainwindow.cpp
@@ -94,7 +94,7 @@ MainWindow::~MainWindow()
 bool MainWindow::queryClose()
 {
     if (!m_quitRequested && qApp && !qApp->isSavingSession()) {
-        hide();
+        this->windowHandle()->hide();
         return false;
     }
 
